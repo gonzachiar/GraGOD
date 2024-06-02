cfe90438d022a893c8bcccae664401fa5a41c003
Federico Bello <fe.debello13@gmail.com>
2024-06-02 15:32:20 -0300

    feat: load swat dataset

--- datasets/swat.py
+++ datasets/swat.py
@@ -1,9 +1,70 @@
 import os
+from typing import Tuple
 
 import pandas as pd
 
+BASE_PATH_DEFAULT = "../datasets_files/swat"
+NAME_TRAIN_DEFAULT = "SWaT_data_train.csv"
+NAME_VAL_DEFAULT = "SWaT_data_val.csv"
 
-def load_dataset(names: list[str], path_to_dataset: str = "../datasets_files/swat"):
+
+def load_swat_df_train(
+    name: str = NAME_TRAIN_DEFAULT, path_to_dataset: str = BASE_PATH_DEFAULT
+) -> Tuple[pd.DataFrame]:
+    """
+    Loads the training dataset from the given path and returns a pandas DataFrame.
+    Args:
+        path_to_dataset: Path to the dataset files.
+    Returns:
+        A pandas DataFrame containing the training dataset.
+    """
+    file = os.path.join(path_to_dataset, name)
+    df_train = pd.read_csv(file)
+    df_train_labels = (df_train["Normal/Attack"] == "Attack").astype(int)
+    df_train = df_train.drop(columns=["Normal/Attack"])
+    return df_train, df_train_labels
+
+
+def load_swat_df_val(
+    name: str = NAME_VAL_DEFAULT, path_to_dataset: str = BASE_PATH_DEFAULT
+) -> Tuple[pd.DataFrame]:
+    """
+    Loads the validation dataset from the given path and returns a pandas DataFrame.
+    Args:
+        path_to_dataset: Path to the dataset files.
+    Returns:
+        A pandas DataFrame containing the validation dataset.
+    """
+    file = os.path.join(path_to_dataset, name)
+    df_val = pd.read_csv(file)
+    df_val_labels = (df_val["Normal/Attack"] == "Attack").astype(int)
+    df_val = df_val.drop(columns=["Normal/Attack"])
+    return df_val, df_val_labels
+
+
+def split_val_df(
+    df_val: pd.DataFrame, df_val_labels: pd.Series, val_size: float = 0.6
+) -> Tuple[pd.DataFrame, pd.Series, pd.DataFrame, pd.Series]:
+    """
+    Split the validation dataset into two parts: validation and test.
+    Args:
+        df_val: The DataFrame with the validation data.
+        df_val_labels: The labels of the validation data.
+        val_size: The size of the validation set.
+    Returns:
+        The validation and test datasets.
+    """
+    val_size = int(val_size * len(df_val))
+    df_test = df_val.iloc[val_size:]
+    df_test_labels = df_val_labels.iloc[val_size:]
+    df_val = df_val.iloc[:val_size]
+    df_val_labels = df_val_labels.iloc[:val_size]
+    return df_val, df_val_labels, df_test, df_test_labels
+
+
+def load_swat_df(
+    path_to_dataset: str = BASE_PATH_DEFAULT, val_size: int = 0.6
+) -> Tuple[pd.DataFrame]:
     """
     Loads the dataset from the given path and returns a pandas DataFrame.
     Args:
@@ -12,9 +73,10 @@ def load_dataset(names: list[str], path_to_dataset: str = "../datasets_files/swa
     Returns:
         A pandas DataFrame containing the dataset.
     """
-    dataframes = []
-    for name in names:
-        file = os.path.join(path_to_dataset, name)
-        dataframes.append(pd.read_csv(file, sep=","))
+    df_train, df_train_labels = load_swat_df_train(path_to_dataset=path_to_dataset)
+    df_val, df_val_labels = load_swat_df_val(path_to_dataset=path_to_dataset)
+    df_val, df_val_labels, df_test, df_test_labels = split_val_df(
+        df_val, df_val_labels, val_size=val_size
+    )
 
-    return pd.concat(dataframes)
+    return df_train, df_train_labels, df_val, df_val_labels, df_test, df_test_labels
--- research/datasets/20240512_swat.py
+++ research/datasets/20240512_swat.py
@@ -5,18 +5,20 @@ import sys
 
 import pandas as pd
 
-day = "29"
-month = "June"
-year = "2020"
-date = f"{day}{month}{year}"
 path_to_dataset = "../../datasets_files/swat"
-file = os.path.join(path_to_dataset, f"{date}.csv")
-df_swat = pd.read_csv(file, sep=",")
-df_swat_temp = pd.read_excel("../../datasets_files/swat/29June2020 (1).xlsx")
+df_train = pd.read_csv(os.path.join(path_to_dataset, "SWaT_Dataset_Normal_v1.csv"))
+df_val = pd.read_csv(os.path.join(path_to_dataset, "SWaT_Dataset_Attack_v0.csv"))
 
 # %%
-df_swat
+df_train.head()
 # %%
-df_swat_temp = pd.read_excel("../../datasets_files/swat/29June2020 (1).xlsx")
+df_val.head()
+# %%
+(df_train["Normal/Attack"] == "Normal").value_counts()
+# %%
+(df_val["Normal/Attack"] == "Attack").value_counts()
+
 
 # %%
+def load_swat_dataset():
+    return df_train, df_val
